c8c5d8b235ffb6bfbf44371b47d87f8ac2dc76b2
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-22 21:55:46 +0700

    membuat detailOrder_page menggunakan StatefulWidget

---
+++ lib/presentation/detailOrder_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderPage extends StatefulWidget {
+  const DetailOrderPage({super.key});
+
+  @override
+  State<DetailOrderPage> createState() => _DetailOrderPageState();
+}
+
+class _DetailOrderPageState extends State<DetailOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
