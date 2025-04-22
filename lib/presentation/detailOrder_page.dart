134c543b51616df3b9c422383360f21b57e1dbaf
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-22 22:15:51 +0700

    menambahkan detail pesanan dan tombol selesai

--- lib/presentation/detailOrder_page.dart
+++ lib/presentation/detailOrder_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
 
-class DetailOrderPage extends StatefulWidget {
+class DetailOrderPage extends StatelessWidget {
   final String makanan;
   final String minuman;
   final String jumlahMakanan;
@@ -14,14 +15,39 @@ class DetailOrderPage extends StatefulWidget {
     required this.jumlahMinuman,
     required this.totalHarga
   });
-  
-  @override
-  State<DetailOrderPage> createState() => _DetailOrderPageState();
-}
 
-class _DetailOrderPageState extends State<DetailOrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food  QYT Order: $jumlahMakanan'),
+            Text('Drink QYT Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
