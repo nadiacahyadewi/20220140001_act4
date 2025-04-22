a1b1710ff2b087761ce635cc0f2c152aacf30e97
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-04-22 21:58:27 +0700

    membuat button order

--- lib/presentation/order_page.dart
+++ lib/presentation/order_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/detailOrder_page.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
@@ -73,8 +74,30 @@ class _OrderPageState extends State<OrderPage> {
                 return null;
               },
             ),
+            ElevatedButton(
+              onPressed: (){
+                if (_formKey.currentState!.validate()) {
+                  calculateTotalPrice();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: 
+                          (context) => DetailOrderPage(
+                            jumlahMakanan: jumlahMakananController.text,
+                            jumlahMinuman: jumlahMinumanController.text,
+                            makanan: makananController.text,
+                            minuman: minumanController.text,
+                            totalHarga: totalHarga,
+                          ),
+                        
+                  ),
+                );
+              }
+            },
+            child: Text('Order Now'),
+            ),
           ],
-        ) 
+        ),
       ),
     );
   }
